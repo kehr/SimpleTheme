3897e452838d2ece566035e7a50d6663daa8285b
kehr <kehr.china@gmail.com>
2014-10-03 23:24:59 +0800

    update

--- assets/js/time.js
+++ assets/js/time.js
@@ -3,7 +3,7 @@ function startTime()
 {
 var today=new Date()
 var basetime01=today.getTime()
-var basetime02=Date.parse("Aug 30, 2014")
+var basetime02=Date.parse("Aug 2, 2012")
 var basetime=Math.round((basetime01 - basetime02)/(1000*60*60))
 var h=today.getHours()+basetime
 var m=today.getMinutes()
@@ -11,7 +11,7 @@ var s=today.getSeconds()
 // add a zero in front of numbers<10
 m=checkTime(m)
 s=checkTime(s)
-document.getElementById('wait-time').innerHTML="已坚守"+h+"小时"+m+"分钟"+s+"秒&nbsp;"
+document.getElementById('wait-time').innerHTML="2012.8  已坚守"+h+"小时"+m+"分钟"+s+"秒&nbsp;"
 t=setTimeout('startTime()',1000)
 }
 
